e4b7c18c82c8325b8bf1905649e9052db54cb8a6
Pranav Byakod <pranav.byakod@gmail.com>
2021-07-27 18:12:46 -0700

    connection and initialize funciton

--- index.js
+++ index.js
@@ -1,2 +1,53 @@
 const inquirer = require('inquirer');
 const mysql2 = require('mysql2');
+const {defaultQ, addDeptQ, addRoleQ, addEmpQ, updateEmpQ} = require('./questions/questions');
+
+const connect = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'trackerDB',
+    password: ''
+});
+
+function initialize() {
+    inquirer.prompt(defaultQ)
+    .then(response => {
+        response = response.userChoice;
+        switch(response) {
+            case 'View Departments':
+                displayDept();
+                break;
+            case 'View Roles':
+                displayRoles();
+                break;
+            case 'View Employees':
+                displayEmp();
+                break;
+            case 'Add Department':
+                additionalDept();
+                break;
+            case 'Add Role':
+                additionalRole();
+                break;
+            case 'Add Employee':
+                additionalEmp();
+                break;
+            case 'Update Employee Role':
+                updateEmpRole();
+                break;
+            case 'Exit':
+                console.log("Thanks for visiting!");
+                break;
+            default:
+                break;
+        }
+    })
+    .catch(error => {
+        if(error) {
+            console.log('ERROR!', error);
+        }
+    })
+};
+
+initialize();
+
