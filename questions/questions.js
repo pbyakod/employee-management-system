9ea36d5db08fcb58a58eb49d69e323a68ed7d0e9
Pranav Byakod <pranav.byakod@gmail.com>
2021-07-27 17:45:57 -0700

    initial prompt

---
+++ questions/questions.js
@@ -0,0 +1,9 @@
+const defaultQ = [
+    {
+        type: 'list',
+        message: 'Welcome! Please select from the following options:',
+        choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Exit'],
+        name: 'userChoice'
+    }
+];
+
