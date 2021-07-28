058a62ed42124875ed009c8400f043a0d3a5aa9a
Pranav Byakod <pranav.byakod@gmail.com>
2021-07-27 20:05:59 -0700

    adding roles and depts

--- index.js
+++ index.js
@@ -77,3 +77,66 @@ function displayRoles() {
             console.log('ERROR!', error);
         })
 }
+
+function displayEmp() {
+    var queryStr = `SELECT emp.id, emp.first_name, emp.last_name, role.title AS title, department.name AS department, role.salary AS salary, (SELECT CONCAT(first_name, ' ', last_name) FROM employee WHERE id = emp.manager_id) AS manager FROM employee emp INNER JOIN role ON emp.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY emp.id;`
+    connect.promise().query(queryStr)
+        .then(emp => {
+            console.log('\nDISPLAYING ALL EMPLOYEES:');
+            console.log(emp[0]);
+            initialize();
+        })
+        .catch(error => {
+            console.log('ERROR!', error);
+        })
+}
+
+function additionalDept() {
+    inquirer.prompt(addDeptQ)
+        .then(response => {
+            response = response.newDept;
+            connect.query(`INSERT INTO department (name) VALUES ('${response}')`, function (error,result) {
+                if(error) {
+                    throw error;
+                }
+                console.log(`${response} has been added to the database!`)
+                initialize();
+            });
+        })
+}
+
+function additionalRole() {
+    connection.promise().query('SELECT * FROM department')
+    .then(role => {
+      var rolesTbl = role[0];
+      var index = 0;
+      while(index < rolesTbl.length) {
+          addRoleQ[2].choices.push(rolesTbl[i].name);
+          index += 1;
+      }
+      inquirer.prompt(addRoleQ)
+      .then(response => {
+        var deptName = response.newDept;
+        connection.promise().query(`SELECT * FROM department WHERE name = '${deptName}'`)
+        .then(dept => {
+          var deptId = dept[0][0].id;
+          connection.query(`INSERT INTO role (title, salary, department_id) VALUE ('${answer.roleName}', ${Number(answer.roleSalary)}, ${deptId})`, error => {
+            if(error) {
+                throw error;
+            }
+            console.log(`${response} has been added to the database!`);
+            initialize();
+          })
+        })
+        .catch(error => {
+          console.log('ERROR!', error)
+        })
+      })
+      .catch(error => {
+        console.log('ERROR!', error)
+      })
+    })
+    .catch(err => {
+      console.log('ERROR!', error)
+    })
+}
