f7ec9af0a7c52274bfb59eaed8b149cb2c8420cd
Pranav Byakod <pranav.byakod@gmail.com>
2021-07-27 17:46:59 -0700

    addDeptQ, addRoleQ, and addEmpQ

--- questions/questions.js
+++ questions/questions.js
@@ -7,3 +7,54 @@ const defaultQ = [
     }
 ];
 
+const addDeptQ = [
+    {
+      type: 'input',
+      message: 'Please enter the name of the new department.',
+      name: 'newDept'
+    }
+];
+
+const addRoleQ = [
+    {
+      type: 'input',
+      message: 'Enter the role name.',
+      name: 'roleName'
+    },
+    {
+      type: 'input',
+      message: 'Enter the expected salary for the role.',
+      name: 'roleSalary'
+    },
+    {
+      type: 'list',
+      message: 'Enter the department assigned for the role.',
+      choices: [],
+      name: 'deptName',
+    }
+]
+
+const addEmpQ = [
+    {
+      type: 'input',
+      message: `Enter the employee's first name.`,
+      name: 'empFirst'
+    },
+    {
+      type: 'input',
+      message: `Enter the employee's last name`,
+      name: 'empLast'
+    },
+    {
+      type: 'list',
+      message: "Enter the employee's role",
+      name: 'empRole',
+      choices: []
+    },
+    {
+      type: 'list',
+      message: "Enter the employee's manager",
+      choices: [{name: 'None', value: null}],
+      name: 'empManager',
+    }
+]
